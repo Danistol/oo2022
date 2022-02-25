c7439e11933ccb5ac8bc9decb1c91767a0573ddd
Danistol <raigosei@tlu.ee>
2022-02-25 08:30:19 +0200

    kt

---
+++ 01_funktsioon/Funktsioonid.java
@@ -0,0 +1,15 @@
+public class Funktsioonid {
+    public static void main(String[] args) {
+        System.out.println(taksoSoit(3.5));
+
+        double soiduMaksumu = taksoSoit(0.5)
+        System.out.println(soiduMaksumu);
+    }
+
+    public static double taksoSoit(double h){
+        return 3+0.8*h;
+    }
+
+    public static void
+}
+
---
+++ 01_funktsioon/src/com/company/KoduT.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class KoduT {
+
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Integer input = scanner.nextInt();
+        int uus = input + 10;
+        System.out.println("Kümne aasta pärast oled"+" "+uus+" "+"aastat vana");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input + " " + "tervist");
+    }
+}
